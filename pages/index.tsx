4119255c96447a5790afe5a8801744abb393d8aa
Vaughn Akiu <vaughnakiu@gmail.com>
2025-02-13 12:10:47 -0700

    making login button look a little better / uniform

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,18 +9,46 @@ const siteTitle = 'Learn Japanese';
 export default function Home({  }) {
   const { data: session } = useSession();
 
-  if (session) {
+  // if (session) {
     return (
       <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        {session ? 
+          
+          <div className={utilStyles.containerLogin} onClick={() => signOut()}>
+            <p>Signed in as {session.user.name}. </p>
+            <p>Click to sign out...</p>
+          </div>
+          
+        : 
+          <div className={utilStyles.containerLogin} onClick={() => signIn()}>
+              <h3>Sign in </h3>
+          </div>
+        }
+        <Head>
+            <title>{siteTitle}</title>
+        </Head>
+        <div className={utilStyles.container}>
+         
+                <h2 className={utilStyles.headingLg}>
+                    Kanji memorization aid
+                </h2>
+                <li>
+                <Link href="chooseKanji">Choose Kanji</Link>
+                </li>
+                <p>pick the kanji you want to show up in your flash card deck...</p>
+                <li>
+                  <Link href="flashCards">Flash Cards</Link>
+                </li>
+                <p>go to your flash card deck...</p>
+         
+        </div>
+        {/* <Layout></Layout> */}
       </>
     )
-  }
+  // }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+
     </> )
 
 
